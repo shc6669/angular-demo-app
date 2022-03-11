8d983f5fc27912f3ba421dbd385e2809e6c17514
Chandrayana Putra <shc6669@gmail.com>
2022-03-11 21:12:06 +0800

    some bug fix 3

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { AuthService, UserType } from '../../modules/auth';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent implements OnInit {
-  constructor() {}
+  user$: Observable<UserType>;
 
-  ngOnInit(): void {}
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.user$ = this.auth.currentUserSubject.asObservable();
+  }
 }
