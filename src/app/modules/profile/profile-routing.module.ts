7ce187e01b8924a40d2b556571f5fd8c9d130105
Chandrayana Putra <shc6669@gmail.com>
2022-03-11 20:37:43 +0800

    some bug fix 2

--- src/app/modules/profile/profile-routing.module.ts
+++ src/app/modules/profile/profile-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
-import { CampaignsComponent } from './campaigns/campaigns.component';
-import { DocumentsComponent } from './documents/documents.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { ProfileComponent } from './profile.component';
-import { ConnectionsComponent } from './connections/connections.component';
 
 const routes: Routes = [
   {
@@ -15,22 +11,6 @@ const routes: Routes = [
       {
         path: 'overview',
         component: OverviewComponent,
-      },
-      {
-        path: 'projects',
-        component: ProjectsComponent,
-      },
-      {
-        path: 'campaigns',
-        component: CampaignsComponent,
-      },
-      {
-        path: 'documents',
-        component: DocumentsComponent,
-      },
-      {
-        path: 'connections',
-        component: ConnectionsComponent,
       },
       { path: '', redirectTo: 'overview', pathMatch: 'full' },
       { path: '**', redirectTo: 'overview', pathMatch: 'full' },
